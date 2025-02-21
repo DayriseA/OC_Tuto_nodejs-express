5a82132fc6423755390a1bd6dde46f39f11bc7ef
DayriseA <adrien.maisak@gmail.com>
2025-02-21 11:46:40 +0100

    feat: Modify logics and routes to handle image upload
    - update .gitignore to ignore uploads folder
    - update app.js to serve static files in uploads folder
    - update controllers/stuff.js to handle image upload
    - update routes/stuff.js to handle image upload

--- controllers/stuff.js
+++ controllers/stuff.js
@@ -1,17 +1,20 @@
 const Thing = require('../models/Thing');
 
 exports.createThing = (req, res, next) => {
-  delete req.body._id;
+  const thingObject = JSON.parse(req.body.thing);
+  
+  delete thingObject._id;
+  delete thingObject._userId; // Never trust the client
+  
   const thing = new Thing({
-    title: req.body.title,
-    description: req.body.description,
-    imageUrl: req.body.imageUrl,
-    price: req.body.price,
-    userId: req.body.userId
+      ...thingObject,
+      userId: req.auth.userId,
+      imageUrl: `${req.protocol}://${req.get('host')}/images/${req.file.filename}`
   });
+
   thing.save()
-    .then(() => res.status(201).json({ message: 'Objet enregistré !'}))
-    .catch((error) => res.status(400).json({ error: error }));
+  .then(() => { res.status(201).json({message: 'Objet enregistré !'})})
+  .catch(error => { res.status(400).json( { error })});
 };
 
 exports.getAllThings = (req, res, next) => {
@@ -26,10 +29,26 @@ exports.getOneThing = (req, res, next) => {
     .catch(error => res.status(404).json({ error }));
 };
 
+
 exports.modifyThing = (req, res, next) => {
-  Thing.updateOne({ _id: req.params.id }, { ...req.body, _id: req.params.id })
-    .then(() => res.status(200).json({ message: 'Objet modifié !'}))
-    .catch(error => res.status(400).json({ error })); 
+  const thingObject = req.file ? {
+    ...JSON.parse(req.body.thing),
+    imageUrl: `${req.protocol}://${req.get('host')}/images/${req.file.filename}`
+  } : { ...req.body};
+  
+  delete thingObject._userId;
+
+  Thing.findOne({ _id: req.params.id })
+    .then(thing => {
+      if (thing.userId != req.auth.userId) {
+        res.status(403).json({ error: 'Requête non autorisée' });
+      } else {
+        Thing.updateOne({ _id: req.params.id }, { ...thingObject, _id: req.params.id })
+          .then(() => res.status(200).json({ message: 'Objet modifié !'}))
+          .catch(error => res.status(401).json({ error }));
+      }
+    })
+    .catch(error => res.status(400).json({ error }));
 };
 
 exports.deleteThing = (req, res, next) => {
